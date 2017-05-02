4cdb6cbddab8a3c3dbeb9bae73c0b8682bc1d0de
YuriiKachmar <kachmarani@gmail.com>
2017-05-02 19:29:56 +0300

    TA0010:Create DB Schemas. -add userSchema -add eventSchema

---
+++ db/userSchema.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Yurii on 02.05.2017.
+ */
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: { type: String, required: true, unique: true },
+    password: {type: String, required: true },
+    username: {type: String, required: true },
+    age: String,
+    gender: String,
+    phone: String,
+    homeTown: String,
+    interests: String,
+    subscribed: String,
+    userPhoto: String,
+    createdEvents: String
+});
+
+
+module.exports = mongoose.model('User', UserSchema);
